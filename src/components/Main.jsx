373ce5fb2ac4e0be94f03ed4f06ec5cafaf724af
krish-98 <muralikrishnanm98@gmail.com>
2022-10-04 23:47:03 +0530

    about section completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,37 +1,54 @@
 import React from "react"
+import Dev from "../assets/dev.svg"
 import Profile from "../assets/programming.svg"
 
 const Main = () => {
   return (
-    <div className="px-5">
-      <div className="max-w-[1200px] mx-auto">
-        {/* Header Section */}
-        <section className="flex flex-col items-center gap-6 text-center py-12 md:py-24">
-          <h1 className="font-bold text-5xl leading-tight tracking-wider md:text-[3.5rem]">
-            Bonjour, I'm{" "}
-            <span className="font-bold text-[#7050ef]">Murali Krishnan</span>
-          </h1>
+    <div className="px-5 text-lightBlack">
+      <div className="max-w-[1158px] mx-auto">
+        {/* Hero Section */}
+        <section
+          id="home"
+          className="flex items-center justify-center w-full py-12 md:py-24 lg:px-6 xl:px-0"
+        >
+          <div className="flex flex-col items-center gap-6 w-full text-center lg:items-start lg:text-left">
+            <h1 className="font-bold text-5xl leading-tight tracking-wider md:text-[3.5rem]">
+              Bonjour, I'm{" "}
+              <span className="font-bold text-primary">Murali Krishnan</span>
+            </h1>
 
-          <p className="font-medium underline underline-offset-2 text-[#87879e] md:text-lg md:underline-offset-4">
-            A Frontend Developer
-          </p>
+            <p className="font-medium underline underline-offset-2 text-textGray md:text-lg md:underline-offset-4">
+              Frontend Developer
+            </p>
 
-          <p className="text-sm leading-6 font-semibold tracking-wide md:w-[70%] lg:text-base">
-            I Design and Develop Web apps from blank HTML DOM page to fully
-            functional app using modern-day JavaScript 😍 technologies.
-          </p>
+            <p className="text-sm leading-6 font-semibold tracking-wide md:w-[70%] lg:text-base">
+              I Design and Develop website & web apps. From blank HTML DOM page
+              to fully functional app using modern-day JavaScript 🖤
+              technologies.
+            </p>
+
+            <button className="bg-primary text-white px-8 py-2 rounded-xl lg:py-4 lg:px-12 lg:mt-6 drop-shadow-2xl hover:text-lightBlack hover:bg-white hover:ring-2 hover:ring-primary transition ease-in-out duration-500">
+              Resume
+            </button>
+          </div>
+
+          <div className="hidden lg:block">
+            <img src={Dev} alt="Dev" />
+          </div>
         </section>
 
         {/* About Me Section */}
-        <section id="#about" className="flex flex-col items-center gap-4">
+        <section id="about" className="flex flex-col items-center gap-6">
           <h2 className="text-3xl font-semibold tracking-wide uppercase my-4 lg:text-4xl">
-            About <span className="text-transparent text-[#7050ef]">Me </span>
+            About <span className="text-primary">Me </span>
           </h2>
 
-          <div className="flex flex-col items-center text-center gap-8 lg:flex-row lg:gap-14 lg:text-left">
-            <img className="md:w-[50%]" src={Profile} alt="my profile" />
+          <div className="flex flex-col items-center text-center gap-10 lg:flex-row">
+            <div className="w-full">
+              <img src={Profile} alt="my profile" />
+            </div>
 
-            <p className="text-sm tracking-wide leading-6 font-semibold w-[80%] lg:text-base lg:leading-7">
+            <p className="w-full text-sm tracking-wide leading-6 font-semibold lg:text-base lg:leading-7 lg:text-left">
               Hello World, I am Murali Krishnan. I belong to Madurai, India. I
               have a Bachelor's Degree in Computer Science & Engg. Yet, learned
               everything on the Internet and still learning.
